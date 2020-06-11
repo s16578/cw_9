4b34f0b3db7d668b760c611f2620cf5670e8455f
Adam Daniluk <s16578@pjwstk.edu.pl>
2020-06-11 10:55:37 +0200

    1st commit

--- cw9/Services/IStudentService.cs
+++ cw9/Services/IStudentService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cw9.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace cw9.Services
 {
     interface IStudentService
     {
-
+        public StudentResponse AllStudents();
     }
 }

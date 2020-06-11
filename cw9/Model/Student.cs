5f1ea28a237a4d4fa911f6dc5b92fcca34433c56
Adam Daniluk <s16578@pjwstk.edu.pl>
2020-06-11 10:47:15 +0200

    First commit

---
+++ cw9/Model/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cw9.Model
+{
+    public partial class Student
+    {
+        public string IndexNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int IdEnrollment { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
+        public string Salt { get; set; }
+
+        public virtual Enrollment IdEnrollmentNavigation { get; set; }
+    }
+}
